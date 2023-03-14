40cc250a5ef3931e1517edc2aa21d87c3469e3cc
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-03-14 17:51:54 -0400

    first commit

---
+++ config/Database.php
@@ -0,0 +1,37 @@
+<?php
+class Database
+{
+    private $host = 'localhost';
+    private $port = '5432';
+    private $dbname = 'quotesdb';
+    private $username = 'postgres';
+    private $password = 'postgres';
+    private $conn;
+
+    /*public function __construct()
+    {
+    $this->username = getenv('USERNAME');
+    $this->password = getenv('PASSWORD');
+    $this->dbname = getenv('DBNAME');
+    $this->host = getenv('HOST');
+    $this->port = getenv('PORT');
+    }*/
+
+    public function connect()
+    {
+        if ($this->conn) {
+            return $this->conn;
+        } else {
+            $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
+
+            try {
+                $this->conn = new PDO($dsn, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $this->conn;
+            } catch (PDOException $e) {
+                echo 'Connection Error: ' . $e->getMessage();
+            }
+        }
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <title>Root Page</title>
+</head>
+
+<body>
+
+    <h1>Lindsey Jimenez</h1>
+
+</body>
+
+</html>
