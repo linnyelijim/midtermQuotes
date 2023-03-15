105237f39c07e920385a9ad09889cbd32936d973
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-03-15 16:05:36 -0400

    xx

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -36,7 +36,7 @@ if (isset($_GET['author_id']) !== null) {
     $quotes_arr = $quotes->read_single();
 }
 
-if (isset($_GET['category_id']) !== null) {
+if (isset($_GET['category_id'])) {
     $quotes->category_id = isset($_GET['category_id']) ? $_GET['category_id'] : die();
     $quotes_arr = $quotes->read_single();
 }
