1295d3487fd3b8c09004071880d5daa9e121d5d6
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-03-15 15:24:53 -0400

    xx

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -23,7 +23,7 @@ if (isset($_GET['id'])) {
 
     if ($quotes->quote !== null) {
         //Change to JSON data
-        echo json_encode($quotes_arr);
+        print_r(json_encode($quotes_arr, JSON_NUMERIC_CHECK));
     } else {
         echo json_encode(
             array('message' => 'No Quotes Found')
@@ -31,37 +31,20 @@ if (isset($_GET['id'])) {
     }
 }
 
-
 if (isset($_GET['author_id']) !== null) {
     $quotes->id = isset($_GET['author_id']) ? $_GET['author_id'] : die();
     $quotes_arr = $quotes->read_single();
-
-    echo json_encode($quotes_arr);
-
-} else {
-    echo json_encode(
-        array('message' => 'author_id Not Found')
-    );
 }
 
 if (isset($_GET['category_id']) !== null) {
     $quotes->category_id = isset($_GET['category_id']) ? $_GET['category_id'] : die();
     $quotes_arr = $quotes->read_single();
-
-    echo json_encode($quotes_arr);
-
-} else {
-    echo json_encode(
-        array('message' => 'category_id Not Found')
-    );
 }
 
 if (isset($_GET['author_id']) && isset($_GET['category_id'])) {
     $quotes->id = isset($_GET['author_id']) ? $_GET['author_id'] : die();
-    $quotes_arr->read_single();
-
-    echo json_encode($quotes_arr);
-
+    $quotes->read_single();
 }
 
+echo json_encode($quotes_arr);
 ?>
